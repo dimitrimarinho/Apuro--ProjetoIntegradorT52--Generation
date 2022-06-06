021500c3ee22bea28df7a3e523c7e8bd0a3c1f0d
thaynasaraiva <thaynasaraiva99@gmail.com>
2022-06-06 12:18:58 -0300

    Adicionando post, put, delete no controller

--- Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/controller/CategoriasController.java
+++ Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/controller/CategoriasController.java
@@ -2,11 +2,18 @@ package com.apuro.ecommerce.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +46,18 @@ public class CategoriasController {
 	public ResponseEntity<List<Categorias>> getByArtista(@PathVariable String artista){
 		return ResponseEntity.ok(repository.findAllByArtistaContainingIgnoreCase(artista));
 	}
+	 @PostMapping
+	    public ResponseEntity <Categorias> post (@Valid @RequestBody Categorias categorias){
+	        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categorias));
+	    }
+
+	 @PutMapping 
+	    public ResponseEntity <Categorias> put (@Valid @RequestBody Categorias categorias){
+	        return ResponseEntity.ok(repository.save(categorias));
+	    }
+
+	 @DeleteMapping("/{id}")
+	    public void delete (@PathVariable long id) {
+	        repository.deleteById(id);
+	    }
 }
