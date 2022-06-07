99e3d1213cb94b04a56ff4e27ed6ba18e28fb65b
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-07 12:00:36 -0300

    Atualizando nome da função GetMapping do preco

--- Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/controller/ProdutosController.java
+++ Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/controller/ProdutosController.java
@@ -44,7 +44,7 @@ public class ProdutosController {
 	}
 	
 	@GetMapping("/preco/{preco}")
-	public ResponseEntity<List<Produtos>> getByArtista(@PathVariable double preco){
+	public ResponseEntity<List<Produtos>> getByPrice(@PathVariable double preco){
 		return ResponseEntity.ok(repository.findAllByPrecoContainingIgnoreCase(preco));
 	}
 	 @PostMapping
