e06e5dd6478acc8ae9a4383f23b63029a50ff6ca
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-07 10:50:33 -0300

    Finalizada Repository Produtos

---
+++ Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/repository/ProdutosRepository.java
@@ -0,0 +1,16 @@
+package com.apuro.ecommerce.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apuro.ecommerce.model.Produtos;
+
+@Repository
+public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
+	
+	public List<Produtos> findAllByNomeContainingIgnoreCase(String nome);
+	public List<Produtos> findAllByPrecoContainingIgnoreCase(double preco);
+
+}
