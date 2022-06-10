216ebed9077f9779374462809740afb819126849
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-10 11:47:30 -0300

    Corrigindo antMatchers método configure

--- Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/seguranca/BasicSecurityConfig.java
+++ Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/seguranca/BasicSecurityConfig.java
@@ -29,7 +29,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeHttpRequests()
 		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
