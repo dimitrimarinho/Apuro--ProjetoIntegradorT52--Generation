a9c1f6e1a1b20905887b73dfb71bb723dcd7c360
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-14 10:16:04 -0300

    Corrigindo - Annotation Autowired

--- Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/seguranca/BasicSecurityConfig.java
+++ Projeto Apuro/ecommerce/src/main/java/com/apuro/ecommerce/seguranca/BasicSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.apuro.ecommerce.seguranca;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
 	private UserDetailsService userDetailsService;
 	
 	@Override
