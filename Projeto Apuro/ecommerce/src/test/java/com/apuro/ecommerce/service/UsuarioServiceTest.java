1bc0210c2bab883d26a7847b892553ff41b34427
thaynasaraiva <thaynasaraiva99@gmail.com>
2022-06-28 23:14:57 -0300

    iniciando os testes

---
+++ Projeto Apuro/ecommerce/src/test/java/com/apuro/ecommerce/service/UsuarioServiceTest.java
@@ -0,0 +1,24 @@
+package com.apuro.ecommerce.service;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+
+public class UsuarioServiceTest {
+
+		@Autowired
+		private UsuarioService
+			
+		@Test
+		private void buscarPorIdTest () throws {
+			serviceImpl.buscarPorId(id);
+		}
+}
