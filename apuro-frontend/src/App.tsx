aa62bea4b68943371f20db5455b53508aa2e3899
unknown <stefanymoliveira87@gmail.com>
2022-06-30 15:09:56 -0300

    criando rodapé e sua rota

--- apuro-frontend/src/App.tsx
+++ apuro-frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <Navbar />
+
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}></div>
+      <Footer />
+    </Router>
+
   );
-  
+
 }
 
 export default App;
