2172651a12cef44fafd2cece61983b20c5197296
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-07-04 13:54:15 -0300

    Telas Login e Cadastro

--- apuro-frontend/src/App.tsx
+++ apuro-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/static/navbar/Navbar';
 import Footer from './components/static/footer/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/cadastroUsuario';
 import SobreNos from './pages/sobrenos/SobreNos';
+import './App.css';
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
           <Route path="/sobre-nos" element={<SobreNos />} />
         </Routes>
       </div>
--- apuro-frontend/src/pages/login/Login.tsx
+++ apuro-frontend/src/pages/login/Login.tsx
@@ -1,1 +1,39 @@
 import React from "react";
+import { Grid, Typography, TextField, Button } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link } from 'react-router-dom';
+import "./Login.css";
+
+function Login() {
+
+    return (
+        <Grid container direction='row' justifyContent="center" alignItems="center">
+            <Grid alignItems="center" xs={6}>
+                <Box paddingX={20}>
+                    <form>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos1">Entrar</Typography>
+                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Button type='submit' variant='contained' color='primary' className='button-logar'>
+                                Logar
+                            </Button>
+                        </Box>
+                    </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'> Não tem uma conta?</Typography>
+                        </Box>
+                        <Link to='/cadastro' className='text-decoration-none'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid xs={6} className="imagem">
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Login;
