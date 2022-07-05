3f293caf2259395d01a57c74f7cbf942a71d6d7e
Thayna Saraiva <thaynasaraiva99@gmail.com>
2022-07-05 12:56:41 -0300

    Conexão do login com o backend

---
+++ apuro-frontend/src/model/User.ts
@@ -0,0 +1,12 @@
+interface User {
+   
+    id: number;
+    nome: string;
+    usuario: string;
+    senha: string;
+    foto: string;
+}
+
+export default User;
+
+
--- apuro-frontend/src/pages/login/Login.tsx
+++ apuro-frontend/src/pages/login/Login.tsx
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { ChangeEvent, ChangeEventHandler, useState, useEffect} from "react";
 import { Grid, Typography, TextField, Button } from "@material-ui/core";
 import { Box } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Login.css";
+import UserLogin from "../../model/User.Login";
+import { login } from "../../services/Service";
+import useLocalStorage from "react-use-localstorage";
+
 
 function Login() {
+
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage("token");
+    
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+
+            id: 0,
+            usuario: "",
+            senha: "",
+            foto: "",
+            token: ""
+        })
+
+    function updatedModel (e: ChangeEvent<HTMLInputElement>)  {
+        setUserLogin(
+            {
+               ...userLogin,
+               [e.target.name] : e.target.value
+            })
+    }
+
+    async function onSubmit (e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+           
+            await login ( "/usuario/logar",userLogin,setToken)
+
+            alert("Usuário logado com sucesso!"); //estilizar a mensagem
+            
+        }
+        catch (error) {
+            
+            alert("Dados inconsistentes. Erro ao logar!"); //estilizar a mensagem
+        }
+    }
+
+    useEffect(() => {
+        if (token != "") {
+            navigate('/home')
+        }
+    }, [token])
+
 
     return (
         <Grid container direction='row' justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos1">Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField  value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField  value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Button type='submit' variant='contained' color='primary' className='button-logar'>
                                 Logar
