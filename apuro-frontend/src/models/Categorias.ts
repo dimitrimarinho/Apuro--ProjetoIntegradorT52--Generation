1501221fcde75b946320df5dea606bd996526eee
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-07-17 17:46:24 -0300

    Ajustes no deploy do Backend no Heroku

--- apuro-frontend/src/models/Categorias.ts
+++
@@ -1,8 +0,0 @@
-interface Categorias {
-    id: number;
-    tipo: string;
-    artista: string;
-    tutorial?: string | undefined
-}
-
-export default Categorias;
--- apuro-frontend/src/models/UserLogin.ts
+++
@@ -1,9 +0,0 @@
-interface UserLogin {
-    id: number;
-    usuario: string;
-    senha: string;
-    foto: string;
-    token?: string|null;
-}
-
-export default UserLogin;
