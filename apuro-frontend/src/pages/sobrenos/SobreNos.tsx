48458f2f935c1b6f97a6d85c790aae5da4e911e6
unknown <stefanymoliveira87@gmail.com>
2022-07-18 00:41:51 -0300

    ajustes no sobre nós

--- apuro-frontend/src/pages/sobrenos/SobreNos.tsx
+++ apuro-frontend/src/pages/sobrenos/SobreNos.tsx
@@ -1,7 +1,8 @@
 import { Grid } from '@material-ui/core'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import "./SobreNos.css"
+import { GitHub, LinkedIn } from '@material-ui/icons';
 
 
 function SobreNos() {
@@ -16,35 +17,75 @@ function SobreNos() {
 
                 </Grid>
                 <Grid container className='caixa2'>
-                    <Grid xs={4}>
-                        <Typography variant="h3" color="#192610" align='left'>Quem Somos:</Typography>
-                        <Typography variant="h6" color="#CFD4BC" align="left" >Somos um e-commerce que vende produtos sustentáveis, o qual além de promover a circulação de renda entre camadas periféricas da sociedade brasileira, promove a sustentabilidade </Typography>
+                    <Grid xs={12}>
+                        <Typography variant="h3" className='titulo' >Quem Somos:</Typography>
+                        <Typography variant="h6" className="textoSobre2" >Somos um e-commerce que vende produtos sustentáveis, o qual além de promover a circulação de renda entre camadas periféricas da sociedade brasileira, promove a sustentabilidade. </Typography>
                     </Grid>
-                    <Grid xs={4}>
-                        <Typography variant="h3" color="#192610" align='left'> Motivação:</Typography>
-                        <Typography variant="h6" color="#CFD4BC" align="left">Nossa motivação é a ODS 10, que trata da Redução das desigualdades, devido ao alto impacto que gera para sociedade, pois impacta diretamente nos índices de violência, educação, desemprego, produtividade do trabalho e nível de qualidade de vida da sociedade.
+                    <Grid xs={12}>
+                        <Typography variant="h3" className='titulo'> Motivação:</Typography>
+                        <Typography variant="h6" className="textoSobre2">Nossa motivação é a ODS 10, que trata da Redução das desigualdades, devido ao alto impacto que gera para sociedade, pois impacta diretamente nos índices de violência, educação, desemprego, produtividade do trabalho e nível de qualidade de vida da sociedade.
                         </Typography>
                     </Grid>
-                    <Grid xs={4}>
-                        <Typography variant="h3" color="#192610" align='left'>Público Alvo:</Typography>
-                        <Typography variant="h6" color="#CFD4BC" align='left'>Artistas, principalmente de regiões periféricas, que queiram divulgar seus produtos, a fim de facilitar a venda.</Typography>
+                    <Grid xs={12}>
+                        <Typography variant="h3" className='titulo'>Público Alvo:</Typography>
+                        <Typography variant="h6" className="textoSobre2">Artistas, principalmente de regiões periféricas, que queiram divulgar seus produtos, a fim de facilitar a venda.</Typography>
                     </Grid>
                 </Grid>
                 <Grid xs={12}>
-                    <Typography variant="h3" color="#192610" align="center">Equipe</Typography>
+                    <Typography variant="h3" className="titulo2" align="center">Equipe de Desenvolvedores</Typography>
                 </Grid>
                 <Grid container xs={12}>
-                    <Grid xs={4} className="foto">
-                        <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/11/One-Piece-Sem-Borda.jpg" alt="foto equipe" className="foto" />
+                    <Grid xs={3} alignContent="center" >
+                        <img src="https://i.imgur.com/aCFplnO.jpg" alt="Foto Paulo" className="equipe" />
+                        <Typography variant="inherit" gutterBottom  textAlign="center">Paulo Vitor</Typography>
+                        <Box sx={{ display: "flex", flexWrap: 'wrap', alignItems: "center", justifyContent: "center" }}>
+                        <a href="https://www.linkedin.com/in/paulo-victor-damasceno-e-silva-0184ba183/" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                        <LinkedIn className="icone"/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/paulo-victor-damasceno-e-silva-0184ba183/" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                            <GitHub className="icone"/>
+                            </a>
+                        
+                        </Box>
                     </Grid>
-                    <Grid xs={4}>
-                        <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/11/One-Piece-Sem-Borda.jpg" alt="foto equipe" className="foto" />
+                    <Grid xs={3}>
+                        <img src="https://i.imgur.com/Ms8MutM.jpg" alt="Foto Stefani" className="equipe" />
+                        <Typography variant="inherit" textAlign="center"gutterBottom >Stefany oliveira</Typography>
+                        <Box sx={{ display: "flex", flexWrap: 'wrap', alignItems: "center", justifyContent: "center" }}>
+                        <a href="https://www.linkedin.com/in/stefani-machado-oliveira/" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                        <LinkedIn className="icone"/>
+                            </a>
+                            <a href="https://github.com/stefanimoliveira" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                            <GitHub className="icone"/>
+                            </a>
+                        
+                        </Box>
                     </Grid>
-                    <Grid xs={4}>
-                        <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/11/One-Piece-Sem-Borda.jpg" alt="foto equipe" className="foto" />
+                    <Grid xs={3}>
+                        <img src="https://i.imgur.com/arFPHug.jpg" alt="Foto Dimitri" className="equipe" />
+                        <Typography variant="inherit" gutterBottom textAlign="center">Dimitri Marinho </Typography>
+                        <Box sx={{ display: "flex", flexWrap: 'wrap', alignItems: "center", justifyContent: "center" }}>
+                        <a href="https://www.linkedin.com/in/dimitrimarinho/" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                        <LinkedIn className="icone"/>
+                            </a>
+                            <a href="https://github.com/dimitrimarinho" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                            <GitHub className="icone"/>
+                            </a>
+                       
+                        </Box>
                     </Grid>
-                    <Grid xs={4}>
-                        <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/11/One-Piece-Sem-Borda.jpg" alt="foto equipe" className="foto" />
+                    <Grid xs={3} >
+                        <img src="" alt="Foto Thayna" className="equipe" />
+                        <Typography variant="inherit" className='textoSobre2' textAlign="center" gutterBottom >Thayna Saraiva</Typography>
+                        <Box sx={{ display: "flex", flexWrap: 'wrap', alignItems: "center", justifyContent: "center" }}>
+                        <a href="https://www.linkedin.com/in/thayna-saraiva-2a6061215/" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                        <LinkedIn className="icone"/> 
+                            </a>
+                            <a href="https://github.com/thaynasaraiva" target="_blank" rel="noreferrer" className="text-decorator-none1">
+                            <GitHub className="icone"/>
+                            </a>
+                       
+                        </Box>
                     </Grid>
                 </Grid>
             </Grid>
