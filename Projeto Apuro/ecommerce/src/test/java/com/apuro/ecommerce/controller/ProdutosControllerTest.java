6784f198a920a024ca3b26d4f0b66e9d5562decd
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-09-26 15:55:44 -0300

    Deploy Frontend on Netlify

--- Projeto Apuro/ecommerce/src/test/java/com/apuro/ecommerce/controller/ProdutosControllerTest.java
+++
@@ -1,5 +0,0 @@
-package com.apuro.ecommerce.controller;
-
-public class ProdutosControllerTest {
-
-}
--- Projeto Apuro/ecommerce/src/test/java/com/apuro/ecommerce/repository/ProdutosRepositoryTest.java
+++
@@ -1,5 +0,0 @@
-package com.apuro.ecommerce.repository;
-
-public class ProdutosRepositoryTest {
-
-}
